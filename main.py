a7e05eea37e87ac71d2b61321d88f0660c4a6eba
Aarin Dave <aarindave@gmail.com>
2021-07-04 22:01:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Imports the Pygame module
+# Imports the Pygame module.
 import pygame
 # Imports the constants of Pygame to improve readability.
 from pygame.locals import *
@@ -7,15 +7,16 @@ from random import randint
 
 # Initialize the Pygame font module.
 pygame.font.init()
-# Initialize the Pygame mixer module.
+# Initialize the Pygame music module.
 pygame.mixer.init()
 
-# Assign Constants
+# Set the information for the screen.
 WIDTH = 1200
 HEIGHT = 800
 MID_WIDTH = WIDTH // 2
 MID_HEIGHT = HEIGHT // 2
 
+# Assigns custom RGB values for each color used.
 RED = (230, 60, 80)
 YELLOW = (230, 210, 60)
 GREEN = (60, 230, 110)
@@ -30,27 +31,52 @@ countdown = 3
 team = ""
 player_number = randint(1, 6)
 
-# Load all of the images.
-play = pygame.image.load("Images/play.png")
+# Loads and resizes the menu images.
+play = pygame.image.load("Images/Menu/play.png")
 play = pygame.transform.scale(play, (75, 75))
 
-# Load all of the sounds.
+settings = pygame.image.load("Images/Menu/settings.png")
+settings = pygame.transform.scale(settings, (75, 75))
+
+# Loads and resizes the player images.
+red_player = pygame.image.load("Images/Players/red_player.png")
+red_player = pygame.transform.scale(red_player, (50, 50))
+
+yellow_player = pygame.image.load("Images/Players/yellow_player.png")
+yellow_player = pygame.transform.scale(yellow_player, (50, 50))
+
+green_player = pygame.image.load("Images/Players/green_player.png")
+green_player = pygame.transform.scale(green_player, (50, 50))
+
+blue_player = pygame.image.load("Images/Players/blue_player.png")
+blue_player = pygame.transform.scale(blue_player, (50, 50))
+
+# Loads and resizes the selection images.
+red_logo = pygame.image.load("Images/Selection/red_logo.png")
+# yellow_logo = pygame.image.load("Images/Selection/yellow_logo.png")
+
+green_logo = pygame.image.load("Images/Selection/green_logo.png")
+green_logo = pygame.transform.scale(green_logo, (100, 100))
+
+# blue_logo = pygame.image.load("Images/Selection/blue_logo.png")
+
+# Loads all of the sounds.
 click = pygame.mixer.Sound("Sounds/click.mp3")
 countdown_sound = pygame.mixer.Sound("Sounds/countdown.wav")
 
-# Define Functions
-
 
 def button_clicked(rect_object, circle=False):
-    return event.pos[0] in range(rect_object.left, rect_object.right+1) and \
-           event.pos[1] in range(rect_object.top, rect_object.bottom+1)
+    return event.pos[0] in range(rect_object.left, rect_object.right + 1) and \
+           event.pos[1] in range(rect_object.top, rect_object.bottom + 1)
 
 
+# This is the function to draw the bases.
 def draw_bases():
-    pygame.draw.rect(screen, RED, (0, 0, WIDTH // 2, HEIGHT // 2))
-    pygame.draw.rect(screen, YELLOW, (MID_WIDTH, 0, WIDTH // 2, HEIGHT // 2))
-    pygame.draw.rect(screen, GREEN, (MID_WIDTH, MID_HEIGHT, WIDTH // 2, HEIGHT // 2))
-    pygame.draw.rect(screen, BLUE, (0, MID_HEIGHT, WIDTH // 2, HEIGHT // 2))
+    red_base = pygame.draw.rect(screen, RED, (0, 0, WIDTH // 2, HEIGHT // 2))
+    yellow_base = pygame.draw.rect(screen, YELLOW, (MID_WIDTH, 0, WIDTH // 2, HEIGHT // 2))
+    green_base = pygame.draw.rect(screen, GREEN, (MID_WIDTH, MID_HEIGHT, WIDTH // 2, HEIGHT // 2))
+    blue_base = pygame.draw.rect(screen, BLUE, (0, MID_HEIGHT, WIDTH // 2, HEIGHT // 2))
+    globals().update(locals())
 
 
 def display_text(text, x, y, color, size, center=False):
@@ -68,59 +94,98 @@ def display_menu():
     display_text("Base Wars", WIDTH // 2, HEIGHT // 5, "white", 60, True)
     # Displays the play button
     start_button = screen.blit(play, (WIDTH // 2 - play.get_width() // 2, HEIGHT // 2 - play.get_height() // 2))
+    # settings_button = screen.blit(settings, (WIDTH // 2 - play.get_width() // 2, HEIGHT // 2 - play.get_height() // 2))
     return start_button
 
 
 def display_selection():
-    # Buttons
-    red_choice = pygame.draw.circle(screen, RED, (WIDTH // 5, HEIGHT // 2), 50)  # Red
-    yellow_choice = pygame.draw.circle(screen, YELLOW, (WIDTH // 5 * 2, HEIGHT // 2), 50)  # Yellow
-    green_choice = pygame.draw.circle(screen, GREEN, (WIDTH // 5 * 3, HEIGHT // 2), 50)  # Green
-    blue_choice = pygame.draw.circle(screen, BLUE, (WIDTH // 5 * 4, HEIGHT // 2), 50)  # Blue
+    draw_bases()
 
-    # Text
+    pygame.draw.rect(screen, "white", (100, 300, 1000, 300), border_radius=10)
+
+    # Displays the team choices.
+    red_choice = screen.blit(red_logo, (WIDTH // 5 - 50, HEIGHT // 2 - 50))
+    yellow_choice = pygame.draw.circle(screen, YELLOW, (WIDTH // 5 * 2, HEIGHT // 2), 50)
+    green_choice = screen.blit(green_logo, (WIDTH // 5 * 3 - 50, HEIGHT // 2 - 50))
+    blue_choice = pygame.draw.circle(screen, BLUE, (WIDTH // 5 * 4, HEIGHT // 2), 50)
+
+    # Displays the text of the selection.
     display_text("Choose Your Team", WIDTH // 2, HEIGHT // 10, "white", 60, True)
     display_text("Team Red", WIDTH // 5, HEIGHT // 3 * 2, RED, 30, True)
     display_text("Team Yellow", WIDTH // 5 * 2, HEIGHT // 3 * 2, YELLOW, 30, True)
     display_text("Team Green", WIDTH // 5 * 3, HEIGHT // 3 * 2, GREEN, 30, True)
     display_text("Team Blue", WIDTH // 5 * 4, HEIGHT // 3 * 2, BLUE, 30, True)
+
     return red_choice, yellow_choice, green_choice, blue_choice
 
 
 def display_countdown():
     global countdown, stage
-
+    # Starts
     if countdown == 3:
         countdown_sound.play()
-
+    # Displays the current countdown time.
     display_text(str(countdown), MID_WIDTH, MID_HEIGHT, "white", 100, True)
-
+    # Decreases the countdown by one second.
     countdown -= 1
 
     if countdown == -1:
+        # Switches to the main game.
         stage = "main"
 
 
+# This is the function to display the main game.
 def display_main():
+    # Draws the four bases.
     draw_bases()
-    red_team.reset_team()
+    # Draws the team members for each team.
+    teams["red"].display_team()
+    teams["yellow"].display_team()
+    teams["green"].display_team()
+    teams["blue"].display_team()
+
+    # Iterate through each pressed key and move the player accordingly.
+    keys = pygame.key.get_pressed()
+    if keys[K_w] or keys[K_UP]:
+        teams[team].move_player(player_number, 0, 1)
+    if keys[K_a] or keys[K_LEFT]:
+        teams[team].move_player(player_number, -1, 0)
+    if keys[K_s] or keys[K_DOWN]:
+        teams[team].move_player(player_number, 0, -1)
+    if keys[K_d] or keys[K_RIGHT]:
+        teams[team].move_player(player_number, 1, 0)
+
+    for teammate in teams.values():
+        pass
 
 
-# Classes
+# Creates a button class to easily create buttons.
+class Button:
+    def __init__(self, x, y, width, height, color):
+        self.button = pygame.draw.rect(screen, color, (x, y, width, height))
+
+    def update(self, position, value):
+        if self.button.collidepoint(position):
+            return value
 
 
+# Creates a class to easily create teams.
 class Team:
     def __init__(self, name, size=6):
-        self.name = name.title()
+        self.name = name
         self.size = size
 
-        self.data = {player_id + 1: Rect(randint(0, WIDTH//2-50), randint(0, HEIGHT//2-50), 50, 50) for player_id in
-                     range(self.size)}
+        self.base = globals()[self.name + "_base"]
+        self.image = globals()[self.name + "_player"]
+
+        self.data = {player_id + 1: Rect(randint(self.base.left, self.base.right - 50),
+                                         randint(self.base.top, self.base.bottom - 50), 50, 50)
+                     for player_id in range(self.size)}
 
     def __repr__(self):
         return f"{list(self.data.items())}"
 
-    def add_player(self, rect_object=Rect(0, 0, 0, 0)):
+    def add_player(self, rect_object=Rect(0, 0, 50, 50)):
         self.size += 1
         self.data[self.size] = rect_object
 
@@ -129,28 +194,30 @@ class Team:
         self.data.pop(player)
         self.data = {player_id - (player_id > player): rect_object for player_id, rect_object in self.data.items()}
 
-    def reset_team(self, reset_pos=False):
+    def display_team(self, reset_pos=False):
         if reset_pos:
-            self.data = {player_id + 1: Rect(randint(0, WIDTH//2 - 50), randint(0, HEIGHT//2 - 50), 50, 50) for player_id in
-                         range(self.size)}
+            self.data = {player_id + 1: Rect(randint(0, WIDTH // 2 - 50), randint(0, HEIGHT // 2 - 50), 50, 50)
+                         for player_id in range(self.size)}
+
         for rect_object in self.data.values():
-            pygame.draw.rect(screen, self.name, rect_object)
+            screen.blit(self.image, rect_object.topleft)
+            # pygame.draw.rect(screen, self.name, rect_object)
 
+    def move_player(self, player_id, x_change=0, y_change=0):
+        self.data[player_id].x += x_change
+        self.data[player_id].y -= y_change
+        # return self.data[player_id]
 
-# Setup all four teams.
-red_team = Team("red")
-yellow_team = Team("yellow")
-green_team = Team("green")
-blue_team = Team("blue")
-
-print(red_team)
 
 # Setups the Pygame window with a width of 1200 pixels and a height of 800 pixels.
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # Sets an appropriate caption for the game.
 pygame.display.set_caption("Base Wars")
 
-# Creates a clock to control the framerate
+# Draws the bases as the background for the menu.
+draw_bases()
+
+# Creates a clock to control the frame rate.
 clock = pygame.time.Clock()
 
 # Starts the game loop which controls all of the game logic.
@@ -167,42 +234,29 @@ while True:
         elif event.type == MOUSEBUTTONUP:
             # If the mouse was pressed and released at the same coordinate.
             if event.pos == last_click:
+                # If we are in the menu page.
                 if stage == "menu":
                     if button_clicked(play_button):
                         stage = "selection"
                         click.play()
-                    elif True:
-                        pass
+                # If we are in the selection page.
                 elif stage == "selection":
                     if button_clicked(red_button):
-                        team = red_team
+                        team = "red"
                         stage = "countdown"
                         # click.play()
                     elif button_clicked(yellow_button):
-                        team = yellow_team
+                        team = "yellow"
                         stage = "countdown"
-                        click.play()
+                        # click.play()
                     elif button_clicked(green_button):
-                        team = green_team
+                        team = "green"
                         stage = "countdown"
-                        click.play()
+                        # click.play()
                     elif button_clicked(blue_button):
-                        team = blue_team
+                        team = "blue"
                         stage = "countdown"
-                        click.play()
-        # If the key is released.
-        elif event.type == KEYUP:
-            if event.key in [K_UP, K_w]:
-                pass
-            elif event.key in [K_DOWN, K_s]:
-                pass
-            if event.key in [K_LEFT, K_a]:
-                pass
-            elif event.key in [K_RIGHT, K_d]:
-                pass
-
-    # Fills the screen with a solid, gray color
-    screen.fill("gray65")
+                        # click.play()
 
     # Creates a screen based on the stage
     if stage == "main":
@@ -210,6 +264,10 @@ while True:
     elif stage == "countdown":
         draw_bases()
         display_countdown()
+        # If the countdown is over.
+        if countdown == 0:
+            # Setup each team using the Team class.
+            teams = {"red": Team("red"), "yellow": Team("yellow"), "green": Team("green"), "blue": Team("blue")}
     elif stage == "selection":
         red_button, yellow_button, green_button, blue_button = display_selection()
     elif stage == "menu":
@@ -223,4 +281,3 @@ while True:
         clock.tick(1)
     else:
         clock.tick()
-
